c02b284e93df63f335a467b47e2ca327e4831d99
Nick Melanson <nickm890@gmail.com>
2022-04-28 19:48:54 -0400

    added search functionality

---
+++ public/javascript/search.js
@@ -0,0 +1,25 @@
+async function searchFormHandler(event) {
+  event.preventDefault();
+
+  // reference from main.handlebar: id="search-input" name="search-text"
+  const search = document.querySelector('input[name="search-text"]').value;
+
+  // if search exists, then do GET fetch
+  if (search) {
+    const response = await fetch(`/search/${search}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // if response is okay, then change page to search results
+    if (response.ok) {
+      document.location.replace(`/search/${search}`);
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector("#search-form")
+  .addEventListener("submit", searchFormHandler);
