9e772f86355e50f126883cd2718eb5baf0db59ca
Nick Melanson <nickm890@gmail.com>
2022-05-03 18:48:43 -0400

    pull issues

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 const path = require("path");
 const express = require("express");
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
 const exphbs = require("express-handlebars");
-=======
-const path = require('path');
-const express = require('express');
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
->>>>>>> 9fc4371e95035e804d486aba28fd7b7ccc3596a4
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,9 +19,5 @@ app.use(express.static("public"));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-<<<<<<< HEAD
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-=======
-  app.listen(PORT, ()=> console.log(`Now listening on port ${PORT}`));
->>>>>>> 9fc4371e95035e804d486aba28fd7b7ccc3596a4
 });
